512af9221e6ea3357660fde0a3da4e2a2d460584
Chanathio-nathantong <69070244@kmitl.ac.th>
2026-07-28 21:59:25 +0700

    Delete surprising.py

--- surprising.py
+++
@@ -1,8 +0,0 @@
-""" Surprising """
-total = float(input())
-highest = float(input())
-minimum = max(0, total - 2 * highest)
-if highest - minimum > 2.0:
-    print("Surprising")
-else:
-    print("Not surprising")
