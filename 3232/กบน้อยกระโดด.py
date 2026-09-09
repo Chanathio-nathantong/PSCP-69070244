4b7321e2bb537e083725f7007f2b116afb618efd
Chanathio-nathantong <69070244@kmitl.ac.th>
2026-09-09 17:31:34 +0700

    s
    s

---
+++ 3232/กบน้อยกระโดด.py
@@ -0,0 +1,16 @@
+""" กบน้อยกระโดด """
+x, y = map(int, input().split())
+total = 0
+count = 0
+
+while total < y:
+    if x <= 0:
+        break
+    total += x
+    count += 1
+    x -= 2
+
+if total >= y:
+    print(count)
+else:
+    print("-1")
